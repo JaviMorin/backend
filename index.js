df4a516abbf7fbbb2236a9fa316b58298e463974
JaviMorin <javiermc_23@hotmail.es>
2022-09-23 00:09:21 +0200

    Cambios en el backend Node.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
 
 const app = express()
 
@@ -43,17 +45,15 @@ let notes = [
   })
   
   app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
 
   app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-    } else {
-    response.status(404).end()
-    }
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -62,33 +62,23 @@ let notes = [
   
     response.status(204).end()
   })
-
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
   
   app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
       important: body.important || false,
       date: new Date(),
-      id: generateId(),
-    }
+    })
   
-    notes = notes.concat(note)
-  
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
 
   const unknownEndpoint = (request, response) => {
