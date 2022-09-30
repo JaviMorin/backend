7963d0003abcbc7b3b78235a1cc9ac7508c6e12b
JaviMorin <javiermc_23@hotmail.es>
2022-09-30 23:56:28 +0200

    Ajustes backend

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ let notes = [
       .catch(error => next(error))
   })
   
-  app.post('/api/notes', (request, response) => {
+  app.post('/api/notes', (request, response, next) => {
     const body = request.body
   
     if (body.content === undefined) {
@@ -83,9 +83,12 @@ let notes = [
       date: new Date(),
     })
   
-    note.save().then(savedNote => {
-      response.json(savedNote)
+    note.save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote)
     })
+    .catch(error => next(error))
   })
 
   app.put('/api/notes/:id', (request, response, next) => {
@@ -114,6 +117,8 @@ let notes = [
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
     } 
   
     next(error)
